38b137eb93aea9240ba2c19eb8213e65a1e85258
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-09-05 06:25:00 -0300

    Implement openDataFolder method in DataFolderUtil
    Added a method to open the data folder using an Intent.

--- source/mobile/backend/java/DataFolderUtil.java
+++ source/mobile/backend/java/DataFolderUtil.java
@@ -2,10 +2,25 @@ package mobile.backend.java;
 
 import org.haxe.extension.Extension;
 
+import android.content.Intent;
+import android.net.Uri;
+
 public class DataFolderUtil
 {
 	public static String getDataFolder()
 	{
 		return Extension.mainContext.getExternalFilesDir(null).getAbsolutePath();
+	}
+
+	public static void openDataFolder()
+	{
+		String path = Extension.mainContext.getExternalFilesDir(null).getAbsolutePath();
+		Intent intent = new Intent(Intent.ACTION_VIEW);
+		Uri uri = Uri.parse(path);
+		intent.setDataAndType(uri, "resource/folder");
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+		if (intent.resolveActivityInfo(Extension.mainContext.getPackageManager(), 0) != null)
+			Extension.mainContext.startActivity(intent);
 	}
 }
